3900b12b8456a0b61760138070a567611121f494
Anjali <ianjalijangir@gmail.com>
2019-06-13 23:31:08 +0530

    Changes

--- src/test/java/com/landq/account/unit/TransferBusinessUnitTest.java
+++ src/test/java/com/landq/account/unit/TransferBusinessUnitTest.java
@@ -20,67 +20,57 @@ import com.landq.account.exception.AuthenticationException;
 import com.landq.account.exception.BusinessException;
 import com.landq.account.service.AccountTransferRequest;
 
-
 public class TransferBusinessUnitTest {
-	
-@Mock
-private IAccountDAO accountRepository;
 
+	@Mock
+	private IAccountDAO accountRepository;
 
+	@InjectMocks
+	TransferBusinessUnit transferBusinessUnit = new TransferBusinessUnit();
 
-@InjectMocks
-TransferBusinessUnit transferBusinessUnit=new TransferBusinessUnit();
+	/**
+	 * This init functionality create the object for InjectMocks property.
+	 */
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-/**
-* This init functionality create the object for InjectMocks property.
-*/
-@Before
-public void init() {
-MockitoAnnotations.initMocks(this);
-}
+	private final String VALID_USERNAME = "anjali123";
+	private final String VALID_PASSWORD = "admin";
+	private final String VALID_SENDER_ACCOUNT_NUMBER = "123456";
+	private final String VALID_RECEIVER_ACCOUNT_NUMBER = "987654";
 
+	@Test
+	public void testTransferAmount() throws ApplicationException {
 
-private final String VALID_USERNAME = "anjali123";
-private final String VALID_PASSWORD = "admin";
-private final String VALID_SENDER_ACCOUNT_NUMBER="123456";
-private final String VALID_RECEIVER_ACCOUNT_NUMBER="9876543";
-  
-@Test
-public void testTransferAmountIsZero() throws  ApplicationException {
-	
-	
 		AccountTransferRequest accountTransferRequest = new AccountTransferRequest();
 		accountTransferRequest.setUsername(VALID_USERNAME);
 		accountTransferRequest.setPassword(VALID_PASSWORD);
 		accountTransferRequest.setSenderAccount(VALID_SENDER_ACCOUNT_NUMBER);
 		accountTransferRequest.setReceiverAccount("VALID_RECEIVER_ACCOUNT_NUMBER");
-		accountTransferRequest.setTransferAmount(0.0);
-		
-		Account senderAc=new Account();
+		accountTransferRequest.setTransferAmount(100.0);
+
+		Account senderAc = new Account();
 		senderAc.setAccountNumber(VALID_SENDER_ACCOUNT_NUMBER);
-		senderAc.setBalance(0.0);
+		senderAc.setBalance(200.0);
 		senderAc.setUserName("Anjali");
 		senderAc.setBankName("SBI");
-		
-		Account receiverAc=new Account();
+
+		Account receiverAc = new Account();
 		receiverAc.setAccountNumber(VALID_RECEIVER_ACCOUNT_NUMBER);
 		receiverAc.setBalance(100.0);
 		receiverAc.setUserName("jangir");
 		receiverAc.setBankName("SBI");
-		
-		
+
 		try {
-			Mockito.when(accountRepository.findByAccountNumber(VALID_SENDER_ACCOUNT_NUMBER));
-			transferBusinessUnit.businessValidation(accountTransferRequest);
-			Assert.fail("Exception_Not_Expected");
+			Mockito.when(accountRepository.findByAccountNumber(Mockito.anyString())).thenReturn(senderAc).thenReturn(receiverAc);
+			Mockito.when(accountRepository.save(Mockito.any(Account.class))).thenReturn(senderAc).thenReturn(receiverAc);
+			transferBusinessUnit.businessValidation(accountTransferRequest);	
 		} catch (BusinessException exception) {
-			// Assert compares the actual result of an application with the expected result.
-			Assert.assertNotNull(exception);
-			Assert.assertTrue(exception != null);
-			Assert.assertEquals("Transfer_Amount_InValid", exception.getMessage());
-
+			Assert.fail("Exception_Not_Expected");
 		}
-		Mockito.verify(transferBusinessUnit, Mockito.times(1)).businessValidation(accountTransferRequest);
-}
-
+		Mockito.verify(accountRepository, Mockito.times(2)).findByAccountNumber(Mockito.anyString());
+		Mockito.verify(accountRepository, Mockito.times(2)).save(Mockito.any(Account.class));
+	}
 }
